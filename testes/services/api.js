bb91cd22ff92d022825faff7d19f5aef47017a94
Esclareca <61212452+Esclareca@users.noreply.github.com>
2020-09-18 06:41:30 -0300

    Update api.js

--- testes/services/api.js
+++ testes/services/api.js
@@ -1,10 +1,10 @@
 const axios = require('axios')
 
 const api = axios.create({
-       baseURL: 'http://192.168.29.66:3333' //reid
+     //  baseURL: 'http://192.168.29.66:3333' //reid
     // baseURL: 'http://192.168.0.114:3333'//gj
    // baseURL: 'http://192.168.42.217:3333'//gj2
-    // baseURL: 'https://esclarecabeta.herokuapp.com/'
+     baseURL: 'https://esclarecabeta.herokuapp.com/'
 })
 
 module.exports = api;
